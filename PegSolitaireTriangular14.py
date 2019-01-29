83aa64101b77e41db3a57113b274cb088f1714de
Jason T Baumbach <jatlast@hotmail.com>
2019-01-29 01:55:31 -0500

    The added function BFS_PopulateGameSpaceTreeEdgesOnDemand did find the solution but remain unable to recreate the steps leading to the solution.

--- PegSolitaireTriangular14.py
+++ PegSolitaireTriangular14.py
@@ -26,10 +26,23 @@ from random import shuffle # used to randomize the order in which moves are made
 import queue # for Breadth-First-Search (BFS)
 from collections import defaultdict # for Graph Path to prevent throwing errors for "key not found"
 
+# Start state of game represented as a 9x9 matrix
+mStartState =   [
+                    [-1,-1,-1,-1,1,-1,-1,-1,-1]
+                    , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+                    , [-1,-1,-1,1,-1,1,-1,-1,-1]
+                    , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+                    , [-1,-1,1,-1,0,-1,1,-1,-1]
+                    , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+                    , [-1,1,-1,1,-1,1,-1,1,-1]
+                    , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+                    , [1,-1,1,-1,1,-1,1,-1,1]
+                ]
 # an individual node representing a specific game state
 class GameSpaceNode: 
     def __init__(self, sMove, sMoveType, nSore, mState):
         self.explored = False
+        self.solution = False
         self.move = sMove
         self.moveType = sMoveType
         self.score = nSore
@@ -43,7 +56,7 @@ class GameSpaceNode:
         }
     
     def __str__(self):
-        return "GameSpaceNode(explored=" + str(self.explored) + ", move=" + self.move + ", moveType=" + self.moveType + ", score=" + str(self.score) + ")"
+        return "GameSpaceNode(solution=" + str(self.solution) + ", explored=" + str(self.explored) + ", move=" + self.move + ", moveType=" + self.moveType + ", score=" + str(self.score) + ")"
 
 # deep-copy the given game state
 def CopyState(mCurrentState):
@@ -162,19 +175,7 @@ def AttemptStateChange(mActualState, changeType):
 # However, it does not preserver equal length rows, so it was not usable for subsequent
 #   graph (as a matrix) traversals using BFS nor DFS
 def PopulateGameSpaceTree(GameSpaceTree):
-    mStartState =   [
-                        [-1,-1,-1,-1,1,-1,-1,-1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [-1,-1,-1,1,-1,1,-1,-1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [-1,-1,1,-1,0,-1,1,-1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [-1,1,-1,1,-1,1,-1,1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [1,-1,1,-1,1,-1,1,-1,1]
-                    ]
-    #nodes = len(mStartState)
-    #print(f"nodes={nodes}")
+    global mStartState
 
     ##### Move 0 = Root
     # Insert root node into Game Space Tree
@@ -198,7 +199,7 @@ def PopulateGameSpaceTree(GameSpaceTree):
         , 5 : "Right"
     }
 
-    SolutionFound = False
+    solutionFound = False
     iterations = 0
 #    for row in range(2, 15):
     for row in range(2, 6):
@@ -221,29 +222,17 @@ def PopulateGameSpaceTree(GameSpaceTree):
                     if IsSolved(mNextState):
                         print("Solved:")
                         PrintState(mNextState)
-                        SolutionFound = True
-                        return SolutionFound, iterations
-    return SolutionFound, iterations
+                        solutionFound = True
+                        return solutionFound, iterations
+    return solutionFound, iterations
 #               else:
 #                   print(f"Unable to move {randomMoves[i]} = {dictMoveTypes[randomMoves[i]]}")
 
 # A subsequent attempt to populate the entire game space tree by augmenting the above
 # function PopulateGameSpaceTree() to simultaneously populate a Python dictionary to represent
 # keys as nodes and associated values as a list of the node's connected edges 
-def PopulateGameSpaceTreeAndPaths(GameSpaceTree, dictGameSpacePaths):
-    mStartState =   [
-                        [-1,-1,-1,-1,1,-1,-1,-1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [-1,-1,-1,1,-1,1,-1,-1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [-1,-1,1,-1,0,-1,1,-1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [-1,1,-1,1,-1,1,-1,1,-1]
-                        , [-1,-1,-1,-1,-1,-1,-1,-1,-1]
-                        , [1,-1,1,-1,1,-1,1,-1,1]
-                    ]
-    #nodes = len(mStartState)
-    #print(f"nodes={nodes}")
+def PopulateGameSpaceTreeAndEdges(GameSpaceTree, dictGameSpaceEdges):
+    global mStartState
 
     ##### Move 0 = Root
     # Insert root node into Game Space Tree
@@ -251,7 +240,7 @@ def PopulateGameSpaceTreeAndPaths(GameSpaceTree, dictGameSpacePaths):
     GameSpaceTree.append([]) # add an empty row 0
     GameSpaceTree[0].append(GameSpaceNode("4:4", "Static", 1, mStartState))
 
-#    dictGameSpacePaths["4:4"] = ["root"]
+#    dictGameSpaceEdges["4:4"] = ["root"]
 
     ##### Move 1 = Singular Left Leaf
     # Insert first and only left node into Game Space Tree
@@ -260,7 +249,7 @@ def PopulateGameSpaceTreeAndPaths(GameSpaceTree, dictGameSpacePaths):
     GameSpaceTree.append([]) # add an empty row 1
     GameSpaceTree[1].append(GameSpaceNode(movedFromTo, "Diagonal_Down_Left", 2, mNextState))
 
-    dictGameSpacePaths["4:4"] = [movedFromTo]
+    dictGameSpaceEdges["4:4"] = [movedFromTo]
 
     dictMoveTypes = {
           0 : "Diagonal_Down_Left"
@@ -271,7 +260,7 @@ def PopulateGameSpaceTreeAndPaths(GameSpaceTree, dictGameSpacePaths):
         , 5 : "Right"
     }
 
-    SolutionFound = False
+    solutionFound = False
     iterations = 0
 #    for row in range(2, 15):
     for row in range(2, 6):
@@ -293,17 +282,17 @@ def PopulateGameSpaceTreeAndPaths(GameSpaceTree, dictGameSpacePaths):
                         print(f"Warning: {row} != {scroe - 1} should never happen")
                     GameSpaceTree[score-1].append(GameSpaceNode(movedFromTo, dictMoveTypes[randomMoves[i]], score, mNextState))
                     # add path
-                    if dictGameSpacePaths[mCurrentMove] is None:
-                        dictGameSpacePaths[mCurrentMove] = [movedFromTo]
+                    if dictGameSpaceEdges[mCurrentMove] is None:
+                        dictGameSpaceEdges[mCurrentMove] = [movedFromTo]
                     else:
-                        dictGameSpacePaths[mCurrentMove] += [movedFromTo]
+                        dictGameSpaceEdges[mCurrentMove] += [movedFromTo]
 
                     if IsSolved(mNextState):
                         print("Solved:")
                         PrintState(mNextState)
-                        SolutionFound = True
-                        return SolutionFound, iterations
-    return SolutionFound, iterations
+                        solutionFound = True
+                        return solutionFound, iterations
+    return solutionFound, iterations
 
 # First attempt to traverse the matrix created by PopulateGameSpaceTree() which proved unsuccessful
 # Based on "BFS follows the following steps:"
@@ -358,23 +347,180 @@ def MatrixBFS(unexploredQ):
     return MatrixBFS(unexploredQ)
 #    return unexploredQ
 
+# Attempt to build the graph's edge dictionary on-demand while traversing in BFS fashion
+# Example dictionary entry: '2:5-6:3': ['8:6-8:2', '6:7-2:5'].
+# Note: verticies are labled by the move (i.e., '2:5-6:3') that brought them into their 
+#   current game state
+# Note: The first node in every edge list is the identical to the dictionary entry's key
+# Again, based on "BFS follows the following steps:"
+#   1. Check the starting node 
+#   2. Add its neighbours to the queue.
+#   3. Mark the starting node as explored.
+#   4. Get the first node from the queue / remove it from the queue
+#   5. Check if node has already been visited.
+#   6. If not, go through the neighbours of the node.
+#   7. Add the neighbour nodes to the queue.
+#   8. Mark the node as explored.
+#   9. Loop through steps 4 to 8 until the queue is empty. (Note: Python won't recurse far enough)
+def BFS_PopulateGameSpaceTreeEdgesOnDemand(dictGameSpaceEdges):
+    global mStartState
+
+    # not initially used but still returned for now.
+    solutionFound = False
+    iterations = 0
+    duplicateWarnings = 0
+
+    ##### Move 0 = Root: ignored because the root has only one child node
+
+    ##### Move 1 = Singular Left Leaf
+    # Insert first and only left node into Game Space Tree
+    # Note: because of symmetry, the entire right (or left) half of the state space tree can be ignored
+    stateChanged, movedFromTo, mNextState = AttemptStateChange(mStartState, "Diagonal_Down_Left")
+    gsMoveOne = GameSpaceNode(movedFromTo, "Diagonal_Down_Left", 2, mNextState)
+    dictGameSpaceEdges[movedFromTo] = [gsMoveOne]
+
+    # Create the unexploredQ and add the Singular Left Leaf to the unexploredQ
+    unexploredQ = queue.Queue()
+
+    # 1. Check the starting node (Unnecessary because it's the root)
+    # 2. Add its neighbours to the queue.
+    unexploredQ.put(gsMoveOne) # Add first child node as first unexplored
+    print(f"1: unexploredQ: {unexploredQ.qsize()}")
+
+    while not solutionFound and duplicateWarnings < 10:
+        # 4. Get the first node from the queue / remove it from the queue
+        gsDequeued = unexploredQ.get()
+    #    print(f"1: unexploredQ:{unexploredQ.qsize()} move={gsDequeued.move}")
+
+        # 5. Check if node has already been visited.
+        if not gsDequeued.explored:
+
+            dictMoveTypes = {
+                0 : "Diagonal_Down_Left"
+                , 1 : "Diagonal_Down_Right"
+                , 2 : "Diagonal_Up_Left"
+                , 3 : "Diagonal_Up_Right"
+                , 4 : "Left"
+                , 5 : "Right"
+            }
+
+    #        print(gsDequeued)
+    #        PrintState(gsDequeued.state)
+
+            # randomize the oder in which moves are attempted
+            randomMoves = [0,1,2,3,4,5]
+            shuffle(randomMoves)
+            for i in range(0, 6):
+                stateChanged, movedFromTo, mNextState = AttemptStateChange(gsDequeued.state, dictMoveTypes[randomMoves[i]])
+                if stateChanged:
+                    iterations = iterations + 1
+                    print(f"iterations({iterations}): unexploredQ({unexploredQ.qsize()})")
+                
+                    score = GetScore(mNextState)
+
+                    # 7. Add the neighbour nodes to the queue.
+                    gsNewNode = GameSpaceNode(movedFromTo, dictMoveTypes[randomMoves[i]], score, mNextState)
+                    unexploredQ.put(gsNewNode)
+
+                    # Add all neibour nodes to the edge dictionary
+                    if dictGameSpaceEdges[gsDequeued.move] is None:
+                        dictGameSpaceEdges[gsDequeued.move] = [gsNewNode]
+#                        dictGameSpaceEdges[gsDequeued.move] = [gsDequeued, gsNewNode]
+                    elif gsDequeued.move != gsNewNode.move:
+                        dictGameSpaceEdges[gsDequeued.move] += [gsNewNode]
+                    else:
+                        print("Error: duplicate moves: {gsDequeued.move} == {gsNewNode.move}")
+                        duplicateWarnings = duplicateWarnings + 1
+
+                    if IsSolved(mNextState):
+                        gsNewNode.solution = True
+                        print("Solved:")
+                        PrintState(mNextState)
+                        solutionFound = True
+                        return solutionFound
+
+            # 8. Mark the node as explored.
+            gsDequeued.explored = True
+
+    # return as yet to be decided
+    return solutionFound, iterations
+
+
 # 2D matrix where rows are of equal game scores and columns are increasing game scores
 GameSpaceTree = []
 
-dictGameSpacePaths = defaultdict(list)
+dictGameSpaceEdges = defaultdict(list)
 
-# dictGameSpacePaths['a:b'] = ["a"]
-# if dictGameSpacePaths['a:b'] is None:
-#     dictGameSpacePaths['a:b'] = ["b"]
+BFS_PopulateGameSpaceTreeEdgesOnDemand(dictGameSpaceEdges)
+
+solutionKey = ""
+stepsToSolution = 0
+count = 0
+for key in dictGameSpaceEdges.keys():
+    print(f"key1={key}")
+    count = 0
+    for i in range(0, len(dictGameSpaceEdges[key])):
+        count = count + 1
+        if dictGameSpaceEdges[key][i].solution:
+            solutionKey = key
+            stepsToSolution = count
+            print(f"move={dictGameSpaceEdges[key][i].move} sol={str(dictGameSpaceEdges[key][i].solution)}", end=" ")
+        else:
+            print(f"move={dictGameSpaceEdges[key][i].move}", end=" ")
+    print("\n")
+
+# print solution
+print(f"key={solutionKey} contains the {stepsToSolution} moves leading to the solution")
+for i in range(0, len(dictGameSpaceEdges[solutionKey]) ):
+    PrintState(dictGameSpaceEdges[solutionKey][i].state)
+    if dictGameSpaceEdges[solutionKey][i].solution:
+        print(f"move={dictGameSpaceEdges[solutionKey][i].move} sol={str(dictGameSpaceEdges[solutionKey][i].solution)}")
+    else:
+        print(f"move={dictGameSpaceEdges[solutionKey][i].move}")
+
+
+# dictGameSpaceEdges['a:b'] = ["a"]
+# if dictGameSpaceEdges['a:b'] is None:
+#     dictGameSpaceEdges['a:b'] = ["b"]
 # else:
-#     dictGameSpacePaths['a:b'] += ["b"]
-# print(dictGameSpacePaths['a:b'])
+#     dictGameSpaceEdges['a:b'] += ["b"]
+# print(dictGameSpaceEdges['a:b'])
 
-SolutionWasFound, NumberOfIterations = PopulateGameSpaceTreeAndPaths(GameSpaceTree, dictGameSpacePaths)
-print(dictGameSpacePaths)
+########################################################
+##### Test dictionary storage of GameStateNodes & Q
+def TestDictOfEdges():
+    global mStartState
+
+    gsOne = GameSpaceNode("Initial", "Static", 1, mStartState)
+    gsTwo = GameSpaceNode("Next", "Diagonal_Down_Left", 2, mStartState)
+
+    dictGameSpaceEdges['a:b'] = [gsOne]
+    if dictGameSpaceEdges['a:b'] is None:
+        dictGameSpaceEdges['a:b'] = [gsTwo]
+    else:
+        dictGameSpaceEdges['a:b'] += [gsTwo]
+    #print( list( dictGameSpaceEdges.items() )[0][1].score)
+    print(list(dictGameSpaceEdges.keys()))
+    print(f"len={len(dictGameSpaceEdges['a:b'])}")
+
+    unexploredQ = queue.Queue()
+
+    for key in dictGameSpaceEdges.keys():
+        print(f"key={key}")
+        for i in range(0, len(dictGameSpaceEdges[key]) ):
+            unexploredQ.put(dictGameSpaceEdges[key][i])
+            print(f"score={dictGameSpaceEdges[key][i].score}")
+
+    gsDequeued = unexploredQ.get()
+    print(f"1: unexploredQ:{unexploredQ.qsize()} move={gsDequeued.move}")
+#TestDictOfEdges()
+########################################################
+
+# SolutionWasFound, NumberOfIterations = PopulateGameSpaceTreeAndEdges(GameSpaceTree, dictGameSpaceEdges)
+# print(dictGameSpaceEdges)
 
 #SolutionWasFound, NumberOfIterations = PopulateGameSpaceTree(GameSpaceTree)
-print(f"The tree contains the solution? {SolutionWasFound}. It took {NumberOfIterations} iterations.")
+# print(f"The tree contains the solution? {SolutionWasFound}. It took {NumberOfIterations} iterations.")
 
 # Note: requires PopulateGameSpaceTree() to have been executed first
 def PrintGameSpaceTree(GameSpaceTree):
@@ -394,50 +540,22 @@ def PrintGameSpaceTreeBranchingFactors(GameSpaceTree):
         print(f"Row[{r}]\t has {str(len(GameSpaceTree[r]))} nodes")
     print(f"For a total of {totalNodes} nodes")
 
-PrintGameSpaceTreeBranchingFactors(GameSpaceTree)
+#PrintGameSpaceTreeBranchingFactors(GameSpaceTree)
 
 #####################################################
 ############# Reference External Code ###############
 # This code requires numpy and is only here as reference
-#   https://towardsdatascience.com/depth-breath-first-search-matrix-traversal-in-python-with-interactive-code-back-to-basics-31f1eca46f55
-# Implement BFS
-def Borrowed_BFS(queue=None):
-    global GameSpaceTree
-    current_index = queue.get()
-    print(f"current_index = {current_index}")
-
-    current_x, current_y = current_index[0], current_index[1]
-
-    score = GameSpaceTree[current_y][current_x].score
-
-    if score == 3: return current_x,current_y
-
-    iterations = 0
-
-    while iterations < 10:
-        for n in range(current_x-1,current_x+2):
-            for m in range(current_y-1,current_y+2):
-                print(f"n[{n}]m[{m}]")
-                iterations = iterations + 1
-                if not (n==current_x and m==current_y) \
-                    and n>-1 and m>-1 \
-                    and n<len(GameSpaceTree[n]) and m<len(GameSpaceTree[m]) \
-                    and (n,m) not in queue.queue :
-                        queue.put((n,m))
-                else:
-                    print(f"else: {iterations}, qSize: {queue.qsize()}")
-    return Borrowed_BFS(queue) # currently causes an infinite loop
 #####################################################
 
-PrintGameSpaceTree(GameSpaceTree)
+#PrintGameSpaceTree(GameSpaceTree)
 
 # 4. Queue for BFS
 #start_queue = queue.Queue()
 #start_queue.put((0,0))
 #BFS_results = BFS(start_queue)
 
-start_queue = queue.Queue()
-start_queue.put((0,0))
+#start_queue = queue.Queue()
+#start_queue.put((0,0))
 #MatrixBFS(start_queue)
 #print(f"[{0}][{0}] = {GameSpaceTree[0][0]}")
 #print(f"Results of BFS = {BFS_results}")
